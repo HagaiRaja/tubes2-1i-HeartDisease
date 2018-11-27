68a2123107759eb90da91edc0cc30458dd65939c
thareqmy <thareqmyha@gmail.com>
2018-11-27 21:23:57 +0700

    ml model backend finish

--- dataprocessing.py
+++ dataprocessing.py
@@ -29,23 +29,29 @@ def collectData(res):
     print(user_input)
 
 
+    replacer = [54, 1, 4, 130, 225, 0, 0, 140, 0, 1, 2, 0, 3]
+
+    j = 0
+
     for i in user_input:
         if((i == 'Column1') or (i == 'Column2')):
-            if(user_input[i] == ''):
-                user_input[i] = np.int64(int(-999))
+            if(user_input[i] == '' or user_input[i] == None):
+                user_input[i] = np.int64(replacer[j])
             else:
                 user_input[i] = np.int64(int(user_input[i]))
         elif((i == 'Column4') or (i == 'Column5') or (i == 'Column6') or (i == 'Column8') or (i == 'Column9') or (i == 'Column10')):
-            if(user_input[i] == ''):
-                user_input[i] = np.float64(int(-999))
+            if(user_input[i] == '' or user_input[i] == None):
+                user_input[i] = np.float64(replacer[j])
             else:
-                user_input[i] = np.float64(int(user_input[i]))
+                user_input[i] = np.float64(float(user_input[i]))
 
         else:
-            if(user_input[i] == ''):
-                user_input[i] = np.uint8(int(-999))
+            if(user_input[i] == '' or user_input[i] == None):
+                user_input[i] = np.uint8(replacer[j])
             else:
                 user_input[i] = np.uint8(int(user_input[i]))
+
+        j = j + 1
 
     user_input_dataframe = pd.DataFrame(user_input, index=[0])
 
@@ -56,36 +62,15 @@ def processData(datas):
     #Drop 13 dan 12 karena di data train kami tidak digunakan
 
     #Mendapatkan mean dan modus dari data
-    data_train = pd.read_csv('tubes2_HeartDisease_train.csv')
-    replacer = processTrain(data_train)
-
-
-
-    #Melakukan data cleansing
-    j = 0
-    for i in datas:
-        if (datas[i][0] == -999):
-
-            datas[i] = replacer[j]
-            print(i, datas[i])
-
-        j = j + 1
-
 
     #Melakukan data drop
     del(datas['Column12'])
     del(datas['Column13'])
 
 
-    c3 = (datas['Column3'][0])
-    c7= (datas['Column7'][0])
-    c11 = (datas['Column11'][0])
-    print(c3, c7, c11)
-
-
-    datas['Column3_1'] = 1
-    datas['Column7_0'] = 1
-    datas['Column11_1'] = 1
+    datas['Column3_{}'.format(datas['Column3'][0])] = 1
+    datas['Column7_{}'.format(datas['Column7'][0])] = 1
+    datas['Column11_{}'.format(datas['Column11'][0])] = 1
 
     datas.drop(['Column3'],axis=1, inplace=True)
     datas.drop(['Column7'],axis=1, inplace=True)
@@ -94,52 +79,3 @@ def processData(datas):
 
 
     return(datas)
-
-
-def dropNanOnCol(df, colName) :
-    listDel = []
-    for index, row in df.iterrows():
-        if (row[colName] == "?") :
-            listDel.append(index)
-    return df.drop(listDel)
-
-def countNanCol(df, colName) :
-    count = 0
-    for index, row in df.iterrows():
-        if (pd.isnull(row[colName])) :
-            count += 1
-    return count
-
-def countNanRow(df, rowNum) :
-    count = 0
-    for column in df :
-        if (pd.isnull(df.loc[rowNum][column])) :
-            count += 1
-    return count
-
-def processTrain(data_train):
-    threshold = 8
-
-    listDel = []
-    for index, row in data_train.iterrows():
-        if (countNanRow(data_train, index) > threshold) :
-            listDel.append(index)
-
-    data_train_clean = data_train.drop(listDel)
-
-
-    replacer = []
-
-    changeWithMean = ['Column4', 'Column5', 'Column8', 'Column10', 'Column12']
-    for column in data_train_clean:
-        cleanData = dropNanOnCol(data_train_clean, column)
-        if (column in changeWithMean) :
-            replacer.append(cleanData.median()[column])
-    #         replacer.append(np.asarray(cleanData.loc[:,column], dtype=np.float).mean())
-        else :
-            replacer.append(cleanData.mode()[column])
-
-    # special case
-    replacer[10] = replacer[10][0]
-
-    return(replacer)
